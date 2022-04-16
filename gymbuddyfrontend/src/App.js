99c8e78a588b0fbcf5ebb1a0328545566720d51c
edirie <essedirie@gmail.com>
2022-04-16 15:45:22 -0400

    updated jumboscreen, searchbar and fetched api

--- gymbuddyfrontend/src/App.js
+++ gymbuddyfrontend/src/App.js
@@ -1,13 +1,32 @@
+// import {useEffect} from 'react'
+import Home from './pages/Home';
 import './App.css';
 import Navbar from './components/Navbar';
-import MyHomescreen from './components/Navbar/Homescreen';
+// import axios from 'axios';
+
 
 
 function App() {
+  const axios = require("axios");
+
+  const options = {
+    method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': 'c9bc40e250mshc9a0253036e8abep1a9eccjsn02f8dbdd6992'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
   return (
     <div className= "homescreen">
       <Navbar />
-      <MyHomescreen />
+      <Home />
     </div>
   );
 }
---
+++ gymbuddyfrontend/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyHomescreen from '../../components/Navbar/Homeimage'
+
+function Home() {
+  return (
+    <div>
+        <MyHomescreen />
+    </div>    
+  );
+}
+
+export default Home
