53a54bf1f805564708d5a6f276eaa341c46e1b86
Dahhan_coder <moustafaeldahhan@gmail.com>
2022-04-16 18:44:59 -0400

    fixed index file for the models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
 const Book = require('./User');
-
-module.exports = { Book };
+const Discussion = require('./Discussion');
+const Schedule = require('./Schedule');
+const Workout = require('./Workout');
+module.exports = { Book, Schedule, Discussion, Workout };
