1374ffaa17fa4017fc38fd705333ddedec042ba0
Dahhan_coder <moustafaeldahhan@gmail.com>
2022-04-16 18:39:24 -0400

    models and graphQL schema

---
+++ server/models/Schedule.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+const User = require('./User');
+
+const ScheduleScehma = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  dates: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  user_id: {
+    type: Number,
+    references: {
+        model: User,
+        key: 'id',
+      },
+  },
+  
+});
+
+const Schedule = model('Schedule', ScheduleScehma);
+
+module.exports = Schedule;
