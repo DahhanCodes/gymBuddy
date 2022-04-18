79682cef1e2db1a7ccdc8e337bbcf0a5958a351a
edirie <essedirie@gmail.com>
2022-04-17 21:26:50 -0400

    added hooks, state and effect

--- gymbuddyfrontend/src/App.js
+++ gymbuddyfrontend/src/App.js
@@ -1,28 +1,12 @@
-// import {useEffect} from 'react'
 import Home from './pages/Home';
 import './App.css';
 import Navbar from './components/Navbar';
-// import axios from 'axios';
 
 
 
 function App() {
-  const axios = require("axios");
 
-  const options = {
-    method: 'GET',
-    url: 'https://exercisedb.p.rapidapi.com/exercises',
-    headers: {
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': 'c9bc40e250mshc9a0253036e8abep1a9eccjsn02f8dbdd6992'
-    }
-  };
-  
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
+ 
   return (
     <div className= "homescreen">
       <Navbar />
--- gymbuddyfrontend/src/pages/Home/index.js
+++ gymbuddyfrontend/src/pages/Home/index.js
@@ -1,12 +1,50 @@
-import React from 'react'
-import MyHomescreen from '../../components/Navbar/Homeimage'
+import React from "react";
+import MyHomescreen from "../../components/Navbar/Homeimage";
+import axios from "axios";
+import {useEffect, useState} from 'react'
+
 
 function Home() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect (() => {
+    axios.get('https://exercisedb.p.rapidapi.com/exercises', {
+      headers: {
+        "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+        "X-RapidAPI-Key": "c9bc40e250mshc9a0253036e8abep1a9eccjsn02f8dbdd6992"
+     },
+  })
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  });
+
   return (
     <div>
-        <MyHomescreen />
-    </div>    
+      <MyHomescreen />
+    </div>
   );
 }
 
-export default Home
+export default Home;
+
+  // const options = {
+  //   method: "GET",
+  //   url: "https://exercisedb.p.rapidapi.com/exercises",
+  //   headers: {
+  //     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  //     "X-RapidAPI-Key": "c9bc40e250mshc9a0253036e8abep1a9eccjsn02f8dbdd6992",
+  //   },
+  // };
+
+  // axios
+  //   .request(options)
+  //   .then(function (response) {
+  //     console.log(response.data);
+  //   })
+  //   .catch(function (error) {
+  //     console.error(error);
+  //   });
