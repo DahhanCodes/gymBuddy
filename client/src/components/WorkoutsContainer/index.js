b3bc3cc775564edcd4e3d5afeb9b825d2e165f73
edirie <essedirie@gmail.com>
2022-04-19 19:52:13 -0400

    fixed api

--- client/src/components/WorkoutsContainer/index.js
+++ client/src/components/WorkoutsContainer/index.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+// import "./styles.css"
 
-function WorkoutsContainer() {
+function WorkoutsContainer({ workouts }) {
   return (
-    <div>WorkoutsContainer</div>
-  )
+    <div>
+      {workouts.map((workout) => {
+        return <div>{workout.bodyPart}</div>;
+      })}
+    </div>
+  );
 }
 
-export default WorkoutsContainer
+export default WorkoutsContainer;
+
+
+
+// FUNCTION BELOW BRINGS UP JUST ONE BODY PART AT A TIME
+
+// function WorkoutsContainer({ workouts }) {
+
+//     const [bodyParts, setBodyParts] = useState([]);
+
+//     useEffect(() => {
+//         if (workouts) {
+//             let tempArray = [];
+//             workouts.forEach(workout => {
+//                 if (!tempArray.includes(workout.bodyPart)) {
+//                     tempArray.push(workout.bodyPart);
+//                 }
+//             });
+
+//             setBodyParts(tempArray);
+//         }
+//     }, [workouts])
+
+//   return (
+//     <div>
+//       {bodyParts.map((bodyPart) => {
+//         return <div>{bodyPart}</div>;
+//       })}
+//     </div>
+//   );
+// }
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import Jumbotron  from "../../components/Jumbotron";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import WorkoutsContainer from "../../components/WorkoutsContainer";
 
 function Home() {
   const [workouts, setWorkouts] = useState([]);
@@ -16,44 +17,20 @@ function Home() {
       })
       .then(({ data }) => setWorkouts(data))
       .catch((error) => console.log(error));
+    
       
   },[]);
 
   return (
     <div>
-    
       <Jumbotron />
+      <WorkoutsContainer workouts={workouts} />
     </div>
   );
 }
 
+
 export default Home;
 
-// const options = {
-//   method: "GET",
-//   url: "https://exercisedb.p.rapidapi.com/exercises",
-//   headers: {
-//     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-//     "X-RapidAPI-Key": "c9bc40e250mshc9a0253036e8abep1a9eccjsn02f8dbdd6992",
-//   },
-// };
-
-// axios
-//   .request(options)
-//   .then(function (response) {
-//     console.log(response.data);
-//   })
-//   .catch(function (error) {
-//     console.error(error);
-//   });
-
-//   .then(({ data }) => setWorkouts(data.workouts))
-//   .catch((error) => console.log(error));
-// }, []);
 
 
-{/* <ul>
-{workouts.map((workouts) => (
-  <li key={workouts.id}>{workouts.title}</li>
-))}
-</ul> */}
