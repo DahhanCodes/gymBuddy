4a380b18ae8f55c3a24d96b02fe6f428846419f3
edirie <essedirie@gmail.com>
2022-04-26 16:09:17 -0400

    update

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 function MyNavbar() {
   return (
